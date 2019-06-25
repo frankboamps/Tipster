4ae20a78c9bf4c78a82e7dbe502d86e01e6e8942
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-06-25 13:55:44 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by frankboamps on 6/25/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
