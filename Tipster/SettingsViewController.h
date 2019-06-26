3fe1e2bf0d9bcc899915bf67cf4bbcb51b863f3c
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-06-25 18:05:30 -0700

    Added settings

---
+++ Tipster/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Tipster
+//
+//  Created by frankboamps on 6/25/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tipster/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  Tipster
+//
+//  Created by frankboamps on 6/25/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
